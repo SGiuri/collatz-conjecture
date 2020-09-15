ad9b0781e57dd5c1a49bb2f5435c4f3a87b15aa9
SGiuri <s.giuri@issm.it>
2020-09-15 14:55:26 +0200

    WIP

--- collatz_conjecture.py
+++ collatz_conjecture.py
@@ -1,2 +1,17 @@
 def steps(number):
-    pass
+    steps_counter = 0
+
+    print(number)
+    # number is even:
+    while number != 1:
+
+        if number % 2 == 1:
+            number = number / 2
+            steps_counter += 1
+        # number is odd:
+        else:
+            number = number * 3 + 1
+            steps_counter += 1
+
+        print(number)
+    return steps_counter
